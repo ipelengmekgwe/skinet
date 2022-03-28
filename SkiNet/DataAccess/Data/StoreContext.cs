ba325a639dfbd1b68a9f8d691ccb8fd6bf248199
Ipeleng Mekgwe <Ipeleng.Mekgwe@dotdigital.com>
2022-03-28 02:07:59 +0200

    Initial commit

---
+++ SkiNet/DataAccess/Data/StoreContext.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
