3fa08bdb516bf06261697f117a5e774402be3996
Ipeleng Mekgwe <Ipeleng.Mekgwe@dotdigital.com>
2022-04-28 22:53:34 +0200

    Add breadcrumb

--- SkiNet/client/src/app/shop/shop.component.ts
+++ SkiNet/client/src/app/shop/shop.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { IBrand } from '../shared/models/brand';
 import { IProduct } from '../shared/models/product';
 import { IType } from '../shared/models/productType';
